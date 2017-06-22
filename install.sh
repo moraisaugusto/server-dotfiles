1d3d1894fa43e7851e285263b002927a201a7999
Augusto Morais <aflavio@gmail.com>
2017-06-22 16:42:50 -0300

    fixing animation position

--- install.sh
+++ install.sh
@@ -80,7 +80,7 @@ loaderSpinner()
     local spinstr='|/-\'
     while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
         local temp=${spinstr#?}
-        printf "\t[%c%c] $1" "$spinstr" "$spinstr"
+        printf "\t\t\t[%c%c] $1" "$spinstr" "$spinstr"
         local spinstr=$temp${spinstr%"$temp"}
         sleep $delay
         printf '\b%.0s' {1..80}
